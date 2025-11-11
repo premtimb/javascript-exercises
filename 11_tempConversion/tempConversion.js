ad0a45aa05f05ef64473a7f5d822099615600ad7
Premtim <premtim.berisha@hotmail.com>
2025-11-11 19:56:41 +0100

    Add formula to convert Fahr/Cels

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let celsius = (fah-32) * (5/9);
+  let celsiusRound = Math.round(celsius * 10)/10;
+  return celsiusRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  let fahrenheitRound = Math.round(fahrenheit * 10)/10;
+  return fahrenheitRound;
 };
 
 // Do not edit below this line
