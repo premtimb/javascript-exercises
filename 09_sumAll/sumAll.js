9a73d62ba324555fa8375bd97134303dc6ba3139
Premtim <premtim.berisha@hotmail.com>
2025-11-11 19:14:10 +0100

    Sum of positive numbers in a range

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) { 
+    if(min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)){
+        return 'ERROR';
+    }
+    const maxNumber = Math.max(min,max);
+    const minNumber = Math.min(min,max);
 
+    let sum=0;
+    for(let i=minNumber;i<=maxNumber;i++){
+        sum+=i;
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
