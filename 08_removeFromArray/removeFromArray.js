8a84fbabb3a11f0c8c637bac9b7651a8a4b0645f
Premtim <premtim.berisha@hotmail.com>
2025-11-11 17:55:09 +0100

    Takes a arguments from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    return arr.filter(item => !toRemove.includes(item));
+
 };
 
 // Do not edit below this line
