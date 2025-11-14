5c4a19a4c2e38a1a2195b9e40bfd97f63b20e824
Premtim <premtim.berisha@hotmail.com>
2025-11-14 22:25:08 +0100

    Finding the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest,person) => {   
+    const currentYear = new Date().getFullYear();
+    const oldestAge = (oldest.yearOfDeath ? oldest.yearOfDeath : currentYear) - oldest.yearOfBirth;
+    const personAge = (person.yearOfDeath ? person.yearOfDeath : currentYear) - person.yearOfBirth;
+    
+    return personAge > oldestAge ? person : oldest;
+    
+    });
 
 };
 
